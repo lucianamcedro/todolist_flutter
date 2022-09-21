4cbffc2b778e47aca68564ff0d3c38be40d6c11a
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2022-09-21 20:00:45 -0300

    configurando textform

---
+++ lib/app/core/widget/todo_list_field.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:todolist_provider/app/core/ui/todo_liist_icons.dart';
+
+class TodoListField extends StatelessWidget {
+  final String label;
+  final bool obscureText;
+  final IconButton? suffixIconButton;
+  final ValueNotifier<bool> obscureTextVN;
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+  TodoListField(
+      {Key? key,
+      required this.label,
+      this.obscureText = false,
+      this.suffixIconButton,
+      this.controller,
+      this.validator})
+      : assert(
+          obscureText == true ? suffixIconButton == null : true,
+          'Obscure Text não pode ser enviado em conjunto com SuffixIconButton',
+        ),
+        obscureTextVN = ValueNotifier(obscureText),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscureTextVN,
+      builder: (_, obscureTextValue, child) {
+        return TextFormField(
+          controller: controller,
+          validator: validator,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Colors.red,
+              ),
+            ),
+            isDense: true,
+            suffixIcon: suffixIconButton ??
+                (obscureText == true
+                    ? IconButton(
+                        onPressed: () {
+                          obscureTextVN.value = !obscureTextValue;
+                        },
+                        icon: Icon(
+                          !obscureTextValue
+                              ? TodoLiistIcons.eye_slash
+                              : TodoLiistIcons.eye,
+                          size: 15,
+                        ),
+                      )
+                    : null),
+          ),
+          obscureText: obscureTextValue,
+        );
+      },
+    );
+  }
+}
